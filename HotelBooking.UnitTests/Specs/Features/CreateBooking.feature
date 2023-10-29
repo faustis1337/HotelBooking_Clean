366975cdb5c19e7b806d3de2d0483c2455da8848
Tomáš Peniak <71640446+pennyb0i@users.noreply.github.com>
2023-10-29 20:23:51 +0100

    Added first scenario

---
+++ HotelBooking.UnitTests/Specs/Features/CreateBooking.feature
@@ -0,0 +1,10 @@
+Feature: Create Booking
+    In order to book a room
+    As a customer
+    I need to specify two dates to get a room in that time period
+    
+    Scenario: Booking is before the fully occupied date
+        Given Fully booked date period starts in 7 days and ends in 14 days
+        When The user makes a booking that starts in 1 day and ends in 6 days
+        Then the booking is outside of fully occupied dates and is created
+        
---
+++ HotelBooking.UnitTests/Specs/Steps/CreateBookingSteps.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using HotelBooking.Core;
+using HotelBooking.Core.BindingModels;
+using HotelBooking.Core.Entities;
+using HotelBooking.Core.Interfaces;
+using HotelBooking.Core.Services;
+using Moq;
+using TechTalk.SpecFlow;
+namespace HotelBooking.UnitTests.Specs.Steps;
+
+[Binding]
+public class CreateBookingSteps {
+    private readonly IBookingManager bookingManager;
+    private readonly Mock<IRepository<Booking>> bookingRepository;
+    private readonly Mock<IRepository<Room>> roomRepository;
+    private readonly Mock<IRepository<Customer>> customerRepository;
+    
+    public CreateBookingSteps()
+    {
+        bookingRepository = new Mock<IRepository<Booking>>();
+        roomRepository = new Mock<IRepository<Room>>();
+        customerRepository = new Mock<IRepository<Customer>>();
+        
+        bookingManager = new BookingManager(bookingRepository.Object,roomRepository.Object,customerRepository.Object);
+    }
+
+    [BeforeScenario]
+    public void SetupMocks()
+    {
+        // Inject Rooms 
+        List<Room> rooms = new() {
+            new Room { Id=1, Description="A" },
+            new Room { Id=2, Description="B" },
+        };
+        
+        roomRepository.Setup(x => x.GetAll()).Returns(rooms);
+        
+        // Inject Customers 
+        List<Customer> customers = new ()
+        {
+            new Customer { Id=1, Name= "Bo Benson" , Email = "BB@mail.com"},
+            new Customer { Id=2, Name= "Joe Johnson" , Email = "JoJo@mail.com"},
+            new Customer { Id=3, Name= "Steve Stevenson" , Email = "stoo@mail.com"},
+        };
+        
+        customerRepository.Setup(x => x.GetAll()).Returns(customers);
+        customerRepository.Setup(x => x.Get(1)).Returns(customers[0]);
+        customerRepository.Setup(x => x.Get(2)).Returns(customers[1]);
+        customerRepository.Setup(x => x.Get(3)).Returns(customers[2]);
+    }
+    
+    #region Scenario: Booking is before the fully occupied date
+    
+    [Given(@"Fully booked date period starts in (.*) days and ends in (.*) days")]
+    public void GivenFullyBookedDatePeriodStartsInDaysAndEndsInDays(int p0, int p1)
+    {
+        bookingRepository.Setup(x => x.GetAll()).Returns(new List<Booking> {
+            new() {
+                CustomerId = 1,
+                StartDate = DateTime.Today.AddDays(p0),
+                EndDate = DateTime.Today.AddDays(p1),
+                RoomId = 1,
+                IsActive = true
+            },
+            new() {
+                CustomerId = 2,
+                StartDate = DateTime.Today.AddDays(p0),
+                EndDate = DateTime.Today.AddDays(p1),
+                RoomId = 2,
+                IsActive = true
+            }
+        });
+    }
+    
+    [When(@"The user makes a booking that starts in (.*) day and ends in (.*) days")]
+    public void WhenTheUserMakesABookingThatStartsInDayAndEndsInDays(int p0, int p1)
+    {
+        bookingManager.CreateBooking(new BookingPostBindingModel {
+            StartDate = DateTime.Today.AddDays(p0),
+            EndDate = DateTime.Today.AddDays(p1),
+            CustomerId = 3
+        });
+    }
+    
+    [Then(@"the booking is outside of fully occupied dates and is created")]
+    public void ThenTheBookingIsOutsideOfFullyOccupiedDatesAndIsCreated()
+    {
+        bookingRepository.Verify(repo => repo.Add(It.IsAny<Booking>()));
+    }
+    #endregion
+}
